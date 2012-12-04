f618bd1a6503f5c3eddf16aa543e272d21cfa898
Dennis Schwertel <s@digitalkultur.net>
2012-12-04 17:54:44 +0100

    fixed setup.py test command

--- setup.py
+++ setup.py
@@ -2,20 +2,38 @@
 """
 eukalypse_now
 
-web server for eukalypse.
+web server for eukalypse. 
 
 :copyright: (c) 2012 Dennis Schwertel, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
 
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
 
 tests_require = [
+    'tox'
 ]
 
 dependency_links = [
+#    'http://github.com/kinkerl/eukalypse/tarball/master#egg=eukalypse',
+#    'https://github.com/dcramer/logan/tarball/master#egg=logan'
 ]
+
 
 install_requires = [
     'South==0.7.6',
@@ -27,8 +45,10 @@ install_requires = [
     'eukalypse',
     'easy-thumbnails==1.1',
     'Django==1.4.1',
+#    'kombu==2.4.8',
     'django-celery==3.0.11',
     'celery==3.0.12',
+
 ]
 
 setup(
@@ -43,6 +63,7 @@ setup(
     zip_safe=False,
     install_requires=install_requires,
     tests_require=tests_require,
+    cmdclass={'test': Tox},
     dependency_links = dependency_links,
     license='BSD',
     include_package_data=True,
