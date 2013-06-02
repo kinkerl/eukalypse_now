3780898fa7c0ea59a71e60e044572ec03ac852bf
Dennis Schwertel <d.schwertel@s-v.de>
2013-06-02 11:18:17 +0200

    version dump

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ install_requires = [
 
 setup(
     name='eukalypse_now',
-    version='0.1.1',
+    version='0.2',
     author='Dennis Schwertel',
     author_email='s@digitalkultur.net',
     description='eukalypse web server',
